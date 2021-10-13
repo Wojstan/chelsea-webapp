0b9d6d5c8f274cb568ca7f8723988921100646f9
Wojstan <wpstaniszewski@gmail.com>
2021-10-13 16:00:32 +0200

    Add dependencies and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout } from "antd";
 
-function App() {
+const { Header, Footer, Sider, Content } = Layout;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Sider>side</Sider>
+      <Layout>
+        <Header>header</Header>
+        <Content>content</Content>
+        <Footer>footer</Footer>
+      </Layout>
+    </Layout>
   );
-}
+};
 
 export default App;
